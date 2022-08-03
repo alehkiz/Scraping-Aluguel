4de121edbae4448fe9209dbb332629eaf77d3c81
Alexandre Santos <alexandresoad@hotmail.com>
2022-08-02 21:10:28 -0300

    final

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from config import *
 from data import *
 from rich.console import Console
 import time
-
+import re
 if __name__ == '__main__':
     console = Console()
     rq = Request()
     console.rule('[bold blue]Configurando')
-    console.print('Atualizando os links, para as [blue underline]50 primeiras páginas')
+    num_pages = console.input('Informe a quantidade de páginas que serão acessadas, [gray]se nenhum valor for informado serão considerado 50 páginas, limite de 50:\n')
+    num_pages = ''.join(re.findall('\d', num_pages))
+    num_pages = int(num_pages) if num_pages != '' else 50
+    num_pages = num_pages if num_pages <= 400 else 400
+    console.print(f'Atualizando os links, para as [blue underline]{num_pages} primeiras páginas')
     start_time = time.time()
-    rq.load(0, 50)
+    rq.load(0, num_pages)
     console.print(f'Links atualizados, tempo para carregar os links: [blue bold]{time.time() - start_time} segundos')
     console.rule('[bold blue]Carregando páginas')
     start_time_pages = time.time()
