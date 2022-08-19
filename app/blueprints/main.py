272e19d1a71c35094f6c5b9340b219fffa61f1db
alehkiz <alexandresoad@hotmail.com>
2022-08-18 23:54:03 -0300

    add forms

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, current_app as app, render_template, url_for, redirect
-
+from app.forms.tenement import Tenement
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = Tenement()
+    if form.validate_on_submit():
+        pass
+
+    return render_template('index.html', form=form)
