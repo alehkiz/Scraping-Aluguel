6328aa855cd92e6247cd040436f54cec12cba6e0
alehkiz <alexandresoad@hotmail.com>
2022-08-23 00:41:52 -0300

    Update tenement.py

--- app/forms/tenement.py
+++ app/forms/tenement.py
@@ -7,6 +7,6 @@ class Tenement(FlaskForm):
     bedrooms = IntegerField('Quartos', validators=[NumberRange(0, 100, message='Aceitos apenas números entre 0 e 100')], widget=NumberInput(min=0, max=100, step=1))
     bathrooms = IntegerField('Banheiros', validators=[NumberRange(0, 100, message='Aceitos apenas números entre 0 e 100')], widget=NumberInput(min=0, max=100, step=1))
     parking = IntegerField('Vagas', validators=[NumberRange(0, 100, message='Aceitos apenas números entre 0 e 100')] , widget=NumberInput(min=0, max=100, step=1))
-    area = IntegerField("Area", validators=[DataRequired('Item obrigatório')])
+    area = IntegerField("Area", validators=[NumberRange(0, 100, message='Aceitos apenas números entre 0 e 100')] , widget=NumberInput(min=0, max=100, step=1))
     neighborhood = StringField('Bairro', validators=[DataRequired('Item obrigatório')])
     submit = SubmitField('Enviar')
